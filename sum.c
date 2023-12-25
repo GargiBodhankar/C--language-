b8428e5638f0b8984047d673e9abaf4c6bb9ad21
GargiBodhankar <gargib1808@gmail.com>
2023-12-24 21:19:05 -0800

    added sum of two numbers

---
+++ sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{ 
+    int num1,num2,sum;
+    printf("enter num1\n");
+    scanf("%d",&num1);
+    printf("enter num2\n");
+    scanf("%d",&num2);
+    sum=num1+num2;
+    printf("the sum is %d",sum);
+
+}   
+
+
+
+
